57ecbca698f1864b164ca9832b2a842b1d1dcfd9
Steve Golton <stevegolton@gmail.com>
2015-12-23 21:58:19 +0000

    Simplified algorithm and adde command line input.

--- p5/main.cpp
+++ p5/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <stdint.h>
+#include <stdlib.h>     /* atoi */
 #include <memory.h>
 
 using namespace std;
 
-#define TEST_NUMBER ( 10 )
+#define TEST_NUMBER ( 20 )
 
 bool is_factor( uint64_t qwCandidate, uint64_t qwLargeNum )
 {
@@ -18,26 +19,18 @@ int smallest_prime_factor( uint64_t iNum )
 	uint64_t n = 6;
 
 	if ( is_factor( 2, iNum ) )
-	{
 		return 2;
-	}
 
 	if ( is_factor( 3, iNum ) )
-	{
 		return 3;
-	}
 
 	while ( n-1 <= iNum )
 	{
 		if ( is_factor( n - 1, iNum ) )
-		{
 			return n - 1;
-		}
 
 		if ( is_factor( n + 1, iNum ) )
-		{
 			return n + 1;
-		}
 
 		n += 6;
 	}
@@ -45,49 +38,59 @@ int smallest_prime_factor( uint64_t iNum )
 	return 0;
 }
 
-int main( void )
+int main( int argc, char**argv )
 {
-	uint64_t n;
-	uint64_t f;
-	uint64_t a[TEST_NUMBER + 1];
-	uint64_t b[TEST_NUMBER + 1];
-	uint64_t num;
-	uint64_t index;
+	int *a;
+	int *b;
+	int iCandidate;
+	int iFactor;
 	uint64_t multiple = 1;
+	int iInput;
+	int iBufLen;
 
-	memset( b, 0, sizeof(a) );
-
-	for( num = 2; num <= TEST_NUMBER; num ++ )
+	if ( argc < 2 )
 	{
-		cout << "Testing " << num << endl;
-		n = num;
-		memset( a, 0, sizeof(a) );
+		cout << "Usage: " << argv[0] << " <number>" << endl;
+		return 1;
+	}
 
-		while( ( f = smallest_prime_factor( n ) ) )
-		{
-			n /= f;
-			a[f]++;
-		}
+	iInput = atoi( argv[1] );
+	iBufLen = iInput + 1;
 
-		for( index = 0; index < sizeof(a) / sizeof(a[0]); index++ )
+	a = (int*)malloc( iBufLen * sizeof( int ) );
+	b = (int*)malloc( iBufLen * sizeof( int ) );
+
+	if ( NULL == a || NULL == b  )
+	{
+		return 1;
+	}
+
+	// Clear b in preparation
+	memset( b, 0, iBufLen * sizeof( int ) );
+
+	while( iInput >= 2 )
+	{
+		cout << "Testing " << iInput << endl;
+		iCandidate = iInput;
+		memset( a, 0, iBufLen * sizeof( int ) );
+
+		while( ( iFactor = smallest_prime_factor( iCandidate ) ) )
 		{
-			if (b[index] < a[index])
+			iCandidate /= iFactor;
+			a[iFactor]++;
+
+			if ( b[iFactor] < a[iFactor] )
 			{
-				b[index] = a[index];
+				multiple *= iFactor;
+				b[iFactor]++;
 			}
 		}
+
+		iInput--;
 	}
 
-	for( index = 0; index < sizeof(b) / sizeof(b[0]); index++ )
-	{
-		cout << "[" << index << "]x" << b[index] << endl;
-
-		while ( 0 != b[index] )
-		{
-			multiple *= index;
-			b[index] --;
-		}
-	}
+	free( a );
+	free( b );
 
 	cout << multiple << endl;
 }
